e417ba2eedc8ca7c0bbd073fd872a5bc5a36f177
Mark Jerald Canal <markjeraldc@gmail.com>
2023-03-30 01:31:44 +0800

    :green_apple: DI layer

---
+++ app/src/main/java/com/example/movielocal/di/DatabaseModule.kt
@@ -0,0 +1,44 @@
+package com.example.movielocal.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.movielocal.common.Consts.MOVIE_DATABASE
+import com.example.movielocal.data.model.MovieDetails
+import com.example.movielocal.data.repository.MovieRepositoryImpl
+import com.example.movielocal.data.source.local.MovieDao
+import com.example.movielocal.data.source.local.MovieDatabase
+import com.example.movielocal.domain.repository.MovieRepository
+import com.example.movielocal.domain.usecase.MovieDataMapper
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+    @Provides
+    @Singleton
+    fun provide(@ApplicationContext context: Context) =
+        Room.databaseBuilder(context, MovieDatabase::class.java, MOVIE_DATABASE)
+            .allowMainThreadQueries()
+            .fallbackToDestructiveMigration()
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideMoviesDao(db: MovieDatabase) = db.movieDao()
+
+    @Provides
+    fun provideEntity() = MovieDetails()
+
+    @Provides
+    fun provideMapper() = MovieDataMapper()
+
+    @Provides
+    fun provideMovieRepositories(dao: MovieDao, mapper: MovieDataMapper): MovieRepository {
+        return MovieRepositoryImpl(dao, mapper)
+    }
+}
